1ab631e4563e2c2cb106fe66213eca59b0bb773c
Bancho Petrov <bancho_p@abv.bg>
2015-04-01 12:42:34 +0200

    Played around with the NamingConv

--- src/java/layer2/domain/NamingConv.java
+++ src/java/layer2/domain/NamingConv.java
@@ -12,6 +12,7 @@ package layer2.domain;
 public class NamingConv {
     
     //roles
+    public static final String ROLE = "role";
     public static final String RESELLER = "reseller";
     public static final String PARTNER = "partner";
     public static final String ADMIN = "admin";
@@ -20,7 +21,11 @@ public class NamingConv {
     //attributes
     public static final String USER_NAME = "userName";
     public static final String PASSWORD = "password";
-    public static final String ROLE = "role";
     
+    //stages
+    public static final String STAGE = "stage";
+    public static final String PRE_APPROVED = "pre-approved";
+    public static final String APPROVED = "approved";
+    public static final String FINISHED = "finished";
     
 }
