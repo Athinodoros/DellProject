68e8b59deb29eaf6493a73a000c971245f3100af
Bancho Petrov <bancho_p@abv.bg>
2015-04-01 12:13:37 +0200

    Added some packages
    Added:
    - layer1 package with Servlet
    - layer2 packages with Naming Convention class and three data classes

---
+++ src/java/layer2/domain/NamingConv.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package layer2.domain;
+
+/**
+ *
+ * @author Bancho
+ */
+public class NamingConv {
+    
+    //roles
+    public static final String RESELLER = "reseller";
+    public static final String PARTNER = "partner";
+    public static final String ADMIN = "admin";
+    public static final String SUPER_USER = "superUser"; //not sure if we'll use it
+    
+    //attributes
+    public static final String USER_NAME = "userName";
+    public static final String PASSWORD = "password";
+    
+    
+}
